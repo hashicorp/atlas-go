2408b54b5cbd27b109157843b1f734a85034e9ab
Seth Vargo <sethvargo@gmail.com>
2014-10-27 17:54:48 -0400

    Refactor the Request object to take multiple options

--- client.go
+++ client.go
@@ -1,14 +1,33 @@
 package harmony
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"net/url"
+	"os"
 	"path"
+	"strings"
 )
+
+// RailsError represents an error that was returned from the Rails server.
+type RailsError struct {
+	Errors map[string][]string `json:"errors"`
+}
+
+// Error collects all of the errors in the RailsError and returns a comma-
+// separated list of the errors that were returned from the server.
+func (re *RailsError) Error() string {
+	list := make([]string, 0)
+	for key, errors := range re.Errors {
+		for _, err := range errors {
+			list = append(list, fmt.Sprintf("%s: %s", key, err))
+		}
+	}
+
+	return strings.Join(list, ", ")
+}
 
 // Client represents a single connection to a Harmony API endpoint.
 type Client struct {
@@ -38,7 +57,11 @@ func NewClient(urlString string) (*Client, error) {
 		return nil, err
 	}
 
-	client := &Client{URL: parsedURL}
+	client := &Client{
+		URL:   parsedURL,
+		Token: os.Getenv("HARMONY_TOKEN"),
+	}
+
 	if err := client.init(); err != nil {
 		return nil, err
 	}
@@ -81,43 +104,93 @@ func (c *Client) init() error {
 }
 
 //
-// If a non-200 status code is returned, it is assumed to be an error and an
-// error is returned. The http.Response object is also returned, so furher
-// inspection may be done to determine the response and appropriate action.
-func (c *Client) request(verb, thepath string) (*http.Response, error) {
-	u := *c.URL
-	u.Path = path.Join(c.URL.Path, thepath)
-
-	request, err := http.NewRequest(verb, u.String(), nil)
-	if err != nil {
-		return nil, err
-	}
-
-	response, err := c.HTTPClient.Do(request)
-	if err != nil {
-		return nil, err
-	}
-
-	if response.StatusCode != 200 {
-		var buff bytes.Buffer
-		io.Copy(&buff, response.Body)
-		return response, fmt.Errorf("client: unexpected response code %d:\n%s",
-			response.StatusCode, buff.String())
-	}
-
-	return response, nil
+type RequestOptions struct {
+	Params  map[string]string
+	Headers map[string]string
+	Body    io.Reader
 }
 
-// requestJSON invokes the request function, but then parses the result as JSON.
-// If the request function returns an error, this method returns an error. If
-// the response is not valid JSON, an error is returned.
-func (c *Client) requestJSON(verb, thepath string, out interface{}) error {
-	response, err := c.request(verb, thepath)
-	if err != nil {
-		return err
+// NewRequest creates a new HTTP request using the given verb and sub path.
+func (c *Client) NewRequest(verb, spath string, ro *RequestOptions) (*http.Request, error) {
+	// Ensure we have a RequestOptions struct (since passing nil is an acceptable
+	// use).
+	if ro == nil {
+		ro = new(RequestOptions)
 	}
-	defer response.Body.Close()
 
-	decoder := json.NewDecoder(response.Body)
-	return decoder.Decode(out)
+	// Create a new URL with the appended path
+	u := *c.URL
+	u.Path = path.Join(c.URL.Path, spath)
+
+	// Add the token and other params
+	var params = make(url.Values)
+	if c.Token != "" {
+		params.Add("access_token", c.Token)
+	}
+	for k, v := range ro.Params {
+		params.Add(k, v)
+	}
+	u.RawQuery = params.Encode()
+
+	// Create the request object
+	request, err := http.NewRequest(verb, u.String(), ro.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	// Add any headers
+	for k, v := range ro.Headers {
+		request.Header.Add(k, v)
+	}
+
+	return request, nil
+}
+
+// checkResp wraps http.Client.Do() and verifies that the request was
+// successful. A non-200 request returns an error formatted to included any
+// validation problems or otherwise.
+func checkResp(resp *http.Response, err error) (*http.Response, error) {
+	// If the err is already there, there was an error higher
+	// up the chain, so just return that
+	if err != nil {
+		return resp, err
+	}
+
+	switch i := resp.StatusCode; {
+	case i == 200:
+		return resp, nil
+	case i == 201:
+		return resp, nil
+	case i == 202:
+		return resp, nil
+	case i == 204:
+		return resp, nil
+	case i == 422:
+		return nil, parseErr(resp)
+	case i == 400:
+		return nil, parseErr(resp)
+	case i == 401:
+		return nil, parseErr(resp)
+	default:
+		return nil, fmt.Errorf("client: %s", resp.Status)
+	}
+}
+
+// parseErr is used to take an error json response and return a single string
+// for use in error messages.
+func parseErr(resp *http.Response) error {
+	railsError := &RailsError{}
+
+	if err := decodeBody(resp, &railsError); err != nil {
+		return fmt.Errorf("Error parsing error body: %s", err)
+	}
+
+	return railsError
+}
+
+// decodeBody is used to JSON decode a body into an interface.
+func decodeBody(resp *http.Response, out interface{}) error {
+	defer resp.Body.Close()
+	dec := json.NewDecoder(resp.Body)
+	return dec.Decode(out)
 }
