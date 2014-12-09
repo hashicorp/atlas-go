76520c341ef5fda8756bb06addc648a990cbf928
Armon Dadgar <armon.dadgar@gmail.com>
2014-12-08 17:06:27 -0800

    Remove the build field

--- v1/artifact.go
+++ v1/artifact.go
@@ -25,7 +25,6 @@ type ArtifactVersion struct {
 	Type     string            `json:"artifact_type"`
 	ID       string            `json:"id"`
 	Version  int               `json:"version"`
-	Build    int               `json:"build"`
 	Metadata map[string]string `json:"metadata"`
 	File     bool              `json:"file"`
 	Slug     string            `json:"slug"`
